01d4ff102a64f0d105d89be2399e24de4407e99d
FireDust97 <lautarortiz472@gmail.com>
2023-04-24 03:30:37 -0300

    Se ha implementado JWT al back-end con éxito. La conexión con el front-end también funciona correctamente.

--- src/main/java/com/portfolioargprog/firedust/Controller/PersonaController.java
+++ src/main/java/com/portfolioargprog/firedust/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolioargprog.firedust.Entity.Persona;
 import com.portfolioargprog.firedust.Interface.PersonaServiceInterface;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,18 +25,21 @@ public class PersonaController {
         return PersonaServiceInterface.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         PersonaServiceInterface.savePersona(persona);
         return "Persona creada con éxito";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         PersonaServiceInterface.deletePersona(id);
         return "Persona eliminada con éxito";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                @RequestParam("nombre") String nuevoNombre,
