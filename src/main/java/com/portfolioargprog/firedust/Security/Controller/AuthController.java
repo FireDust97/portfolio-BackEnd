de312f2865e3151b4c00fbb8002be4ee6197ca52
FireDust97 <lautarortiz472@gmail.com>
2023-05-01 17:14:46 -0300

    Se han modificado los CrossOrigin para la correcta autenticación entre el back y el front-end

--- src/main/java/com/portfolioargprog/firedust/Security/Controller/AuthController.java
+++ src/main/java/com/portfolioargprog/firedust/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://backend-portfolioargprog.onrender.com")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
